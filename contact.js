7da949f5215409f01d6a8f0a228965499d9d1e80
Zyrox <anisanissou2003@gmail.com>
2022-10-30 11:44:01 +0100

    made the contact me form work

--- contact.js
+++ contact.js
@@ -8,3 +8,21 @@ humLogo.addEventListener('click',function(){
     navSide.classList.toggle('flex');
     console.log('hi')
     })
+
+
+    // send Email 
+
+    function sendEmail(){
+        Email.send({
+             // i should hide these infos 
+             SecureToken : "71a6c017-5429-4e95-913c-268f78411819",
+            To : 'justzyrox03@gmail.com',
+            From : 'anisanissou2003@gmail.com',
+            Subject : "New Contact",
+            Body : "Name : " + document.querySelector('#name').value 
+            + "<br> Email : " + document.querySelector('#email').value 
+            + "<br> Message : <br> <br>" + document.querySelector('#theText').value 
+        }).then(
+          message => alert("Thank you for reaching out !")
+        );  
+    }
